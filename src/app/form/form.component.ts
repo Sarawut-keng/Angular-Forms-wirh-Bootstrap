b2809d75a9fb5a7ef53e7b28b8ae44358062ef68
Sarawut <srwkk.8@gmail.com>
2021-06-25 00:55:24 +0700

    First commit

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      postalAddress: ['', Validators.required],
+      comments: ''
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  submitForm() {
+    alert(JSON.stringify(this.form.value));
+  };
+
+}
